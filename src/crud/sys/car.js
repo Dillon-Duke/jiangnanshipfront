94dc7797d0a44fa3da1829b75277754087267011
Dillon-Duke <245311344@qq.com>
2020-05-16 18:32:45 +0800

    first commit

---
+++ src/crud/sys/car.js
@@ -0,0 +1,60 @@
+export const tableOption = {
+  border: true,
+  selection: true,
+  index: false,
+  indexLabel: '序号',
+  stripe: true,
+  menuAlign: 'center',
+  menuWidth: 350,
+  align: 'center',
+  refreshBtn: true,
+  searchSize: 'mini',
+  addBtn: false,
+  editBtn: false,
+  delBtn: false,
+  viewBtn: false,
+  props: {
+    label: 'label',
+    value: 'value'
+  },
+  column: [{
+    label: '车辆名称',
+    prop: 'username',
+    search: true
+  }, {
+    label: '性别',
+    prop: 'sex',
+    dicData: [
+      {
+        label: '男',
+        value: 1
+      }, {
+        label: '女',
+        value: 2
+      }
+    ]
+  }, {
+    label: '年龄',
+    prop: 'age'
+  }, {
+    label: '手机号码',
+    prop: 'phone'
+  }, {
+    label: '工号',
+    prop: 'jobNum'
+  }, {
+    label: '状态',
+    prop: 'status',
+    type: 'select',
+    dicData: [
+      {
+        label: '禁用',
+        value: 0
+      }, {
+        label: '正常',
+        value: 1
+      }
+    ]
+
+  }]
+}
---
+++ src/views/common/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mod-home">   
+    <h2>前言</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.mod-home {
+  line-height: 1.5;
+}
+</style>
+
